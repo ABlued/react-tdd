a0a968738ab83d32de4278f4ad272e6ce88b3c4f
ABlued <skwodhkswkd@naver.com>
2022-02-13 01:35:13 +0900

    react-testing-library 기초 세팅

--- src/sum.js
+++
@@ -1,8 +0,0 @@
-export function sum(a, b) {
-  return a + b;
-}
-
-export function sumOf(numbers) {
-  return numbers.reduce((acc, current) => acc + current, 0);
-}
-// 이런 식으로 리팩토링 이후에도 테스트를 실행해서 통과하면 쉽게 리팩토링이 된다.
