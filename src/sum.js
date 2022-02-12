2cb367b3bac2c660e3094f710604a0eaf1da1a8e
ABlued <skwodhkswkd@naver.com>
2022-02-12 17:50:14 +0900

    테스트 케이스로 리팩토링하기

--- src/sum.js
+++ src/sum.js
@@ -3,9 +3,6 @@ export function sum(a, b) {
 }
 
 export function sumOf(numbers) {
-  let result = 0;
-  numbers.forEach((number) => {
-    result += number;
-  });
-  return result;
+  return numbers.reduce((acc, current) => acc + current, 0);
 }
+// 이런 식으로 리팩토링 이후에도 테스트를 실행해서 통과하면 쉽게 리팩토링이 된다.
