7a4440cad48e95862533fa422af4a470bfddbfde
ABlued <skwodhkswkd@naver.com>
2022-02-12 17:45:43 +0900

    describe로 여러 개의 테스트 실행하기

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,11 @@
-function sum(a, b) {
+export function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum;
+export function sumOf(numbers) {
+  let result = 0;
+  numbers.forEach((number) => {
+    result += number;
+  });
+  return result;
+}
