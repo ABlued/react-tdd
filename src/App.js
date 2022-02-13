7c052bc1da2f3c9b83529083527a279390f8fe57
ABlued <skwodhkswkd@naver.com>
2022-02-13 20:25:52 +0900

    todoApp 컴포넌트 및 테스트(todoList 생성, 토글, 삭제 기능 추가)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import TodoApp from './TodoList/TodoApp';
 
 const App = () => {
-  return <Counter />;
+  return <TodoApp />;
 };
 export default App;
