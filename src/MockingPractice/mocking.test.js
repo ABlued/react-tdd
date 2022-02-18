81b9636ba923ca418d9a575699023c208ebf13cc
ABlued <skwodhkswkd@naver.com>
2022-02-18 21:57:50 +0900

    mock함수는 호출인자와 호출 횟수를 알 수 있다.

--- src/MockingPractice/mocking.test.js
+++ src/MockingPractice/mocking.test.js
@@ -10,12 +10,10 @@ mockFn.mockReturnValue('I am a mock!'); // I am a mock!
 
 // mock 함수는 기본적으로 몸체가 없는 함수이다.
 // mockImplemetation() 메소드를 사용하여 함수 몸체를 구현할 수 있다.
-
 mockFn.mockImplementation((name) => `I am ${name}`);
 console.log(mockFn('ablue'));
 
 // 비동기 함수의 resolve와 reject도 구현할 수 있다.
-
 test('async resolve test', async () => {
   const asyncMock = jest.fn().mockResolvedValue(1);
   const result = await asyncMock();
@@ -26,4 +24,18 @@ test('async reject test', async () => {
   const asyncMock = jest.fn().mockRejectedValue(new Error('Async error'));
   const result = await asyncMock();
   console.log(result); // throws "Async error"
+});
+
+// mock함수는 자신이 어떻게 호출이 되었고 몇 번 호출되었는지를 알 수 있다.
+
+test('mock test', () => {
+  const mockFn = jest.fn();
+  mockFn.mockImplementation((name) => `I am ${name}`);
+
+  mockFn('a');
+  mockFn(['a', 'b']);
+
+  expect(mockFn).toBeCalledTimes(2);
+  expect(mockFn).toBeCalledWith('a');
+  expect(mockFn).toBeCalledWith(['a', 'b']);
 });
