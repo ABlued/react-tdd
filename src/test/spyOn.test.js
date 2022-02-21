cba2a94628183f7d8c971148102b6619a235cc0c
ABlued <skwodhkswkd@naver.com>
2022-02-21 21:25:29 +0900

    spyon 함수 실습

---
+++ src/test/spyOn.test.js
@@ -0,0 +1,12 @@
+test('spyOn test', () => {
+  const calculator = {
+    add: (a, b) => a + b,
+  };
+  const spyFn = jest.spyOn(calculator, 'add');
+
+  const result = calculator.add(2, 3);
+
+  expect(spyFn).toBeCalledTimes(1);
+  expect(spyFn).toBeCalledWith(2, 3);
+  expect(result).toBe(5);
+});
